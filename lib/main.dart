94f2f4fb259d1df7bed23e57b8057244e634fac0
Ahmed Kamel <ahmed.kamel99@outlook.com>
2024-07-27 23:40:30 +0400

    Finished Adding to Firebase Firestore.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/home/home_screen.dart';
@@ -8,6 +9,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseFirestore.instance.disableNetwork();
   runApp(MyApp());
 }
 
